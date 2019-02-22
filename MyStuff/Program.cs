000e181a23072fa3fa7ff0967389b333822096dc
Tshegofatso <tmolobe152gmail.com.here>
2019-02-22 18:50:15 +0200

    First Commit: Committed MyStuff and Problems .cs and .csproj files

---
+++ MyStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
