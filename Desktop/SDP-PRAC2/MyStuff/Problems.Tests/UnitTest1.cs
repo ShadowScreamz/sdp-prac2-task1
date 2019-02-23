f0a173703f220be81bec8591951b71bc3f8661fc
Tshegofatso <tmolobe152gmail.com.here>
2019-02-24 01:34:20 +0200

    Fifth Commit: Ran Tests for Problems.Tests.csproj

--- Desktop/SDP-PRAC2/MyStuff/Problems.Tests/UnitTest1.cs
+++ Desktop/SDP-PRAC2/MyStuff/Problems.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Problems;
 
 namespace Tests
 {
@@ -12,7 +13,16 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Arrange
+            Program P = new Program();
+            string result1 = P.Prefix("hello");
+            string result2 = P.Prefix("");
+            string result3 = P.Prefix("what  ....  did you say??  ");
+
+            //Act
+            Assert.AreEqual("5,1:hello", result1);
+            Assert.AreEqual("0,0:", result2);
+            Assert.AreEqual("27,5:what  ....  did you say??  ", result3);
         }
     }
 }
--- Desktop/SDP-PRAC2/MyStuff/Problems/Program.cs
+++ Desktop/SDP-PRAC2/MyStuff/Problems/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Problems
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+          //method that prefixes string with  its length, number of words, and a colon.
+        public string Prefix(string input)
         {
-            Console.WriteLine("Hello World!");
+                //counter to keep track of chars in string
+                int count = 1;
+               int ii = 0; 
+                //output string
+               
+            for(int i=0;i < input.Length; i++)
+            {
+                if (input[i] == ' ' || input[i] == '\n' || input[i] == '\t')     //sentences are made up of different chars
+                {
+                    count++;
+                }
+                 
+            }    
+               ii++;
+                int count2 = 0;
+                foreach (char c in input)
+                {
+                    count2++;
+                }
+               string sam = $"{count2},{count}:{input}";
+            
+           
+             return sam;
+        }
+       
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.Prefix("Enter a sentence"));           // enter an input       
+            //Console.WriteLine(Prefix();          //read input
+            //Console.ReadLine();   
         }
     }
 }
